3a75ffb4028fad93ccb79b868d39c1981b0cd2fe
KennethOlusegun <kennetholusegun@gmail.com>
2022-05-10 13:41:05 -0300

    Primeiros resolvers

--- src/script.js
+++ src/script.js
@@ -2,13 +2,27 @@ import { ApolloServer, gql } from 'apollo-server';
 
 // eslint-disable-next-line prettier/prettier
 const server = new ApolloServer({
-    typeDefs: gql `
+            typeDefs: gql `
 			type Query {
-				hello: String
-			}
-		`
-})
+				id: ID!
+                name: String!
+                age: Int!
+                average: Float!
+                married: Boolean!
+                arrayString: [String]
 
-server.listen(4003).then(({ url }) => {
-    console.log(`Server listening on url ${url}`);
-});
+			}
+		`,
+            resolvers: {
+                Query: {
+                    id: () => '1asd987-9-a',
+                    name: () => 'Kenneth',
+                    age: () => 24,
+                    average: () => 50.54,
+                    married: () => false,
+                    arrayString: () => ['A', 'B'],
+                });
+
+            server.listen(4003).then(({ url }) => {
+                console.log(`Server listening on url ${url}`);
+            });
